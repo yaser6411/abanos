1f6049ef82f976f1875d4ade654b727455602adb
waielelbashir-oss <waiel.elbashir@gmail.com>
2026-06-28 14:23:28 +0200

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "PUT_YOUR_DATABASE_URL_HERE"
+DATABASE_URL = "postgresql://abanos_user:V4lE2IwW152vA0c8JxnUMyA7HMp8OfnC@dpg-d90gp79o3t8c73c8d660-a.oregon-postgres.render.com/abanos"
 
 engine = create_engine(DATABASE_URL)
 
